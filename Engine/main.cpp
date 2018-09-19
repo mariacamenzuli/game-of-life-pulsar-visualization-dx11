1a7074412154d9b86ee2f592981c1991a8aa6a18
Maria Camenzuli <maria.camenzuli93@gmail.com>
2018-09-19 15:30:48 +0100

    Cleaned up error hanldling in main

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -5,33 +5,35 @@
 #include "Win32Application.h"
 #include "D3D11Renderer.h"
 
+void logErrorAndNotifyUser(const std::string& log, const std::string& userNotification) {
+	std::cerr << log << std::endl;
+	std::cerr << userNotification << std::endl;
+	OutputDebugString(std::wstring(log.begin(), log.end()).c_str());
+	OutputDebugString(std::wstring(userNotification.begin(), userNotification.end()).c_str());
+
+	MessageBox(nullptr,
+			   std::wstring(userNotification.begin(), userNotification.end()).c_str(),
+			   L"An Error has occurred",
+			   MB_OK);
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow) {
 	try {
 		Win32Application win32Application("CMP502", hInstance);
+
 		D3D11Renderer d3D11Renderer(win32Application.getWindowHandle());
 		win32Application.assignD3D11Renderer(d3D11Renderer);
+
 		win32Application.showWindow();
 		win32Application.run();
-	}
-	catch (const D3D11RendererException& e) {
-		std::cerr << e.what() << "; Error code: " << e.getErrorCode() << std::endl;
-		std::ostringstream os;
-		os << "Failed to initialize DirectX. Error code: " << e.getErrorCode();
-		auto msgBoxMsg = os.str();
-		MessageBox(nullptr, std::wstring(msgBoxMsg.begin(), msgBoxMsg.end()).c_str(), L"D3D11 Renderer Error", MB_OK);
+	} catch (const D3D11RendererException& e) {
+		logErrorAndNotifyUser(e.what(), "Failed to initialize DirectX. Error code " + std::to_string(e.getErrorCode()));
 		return EXIT_FAILURE;
-	}
-	catch (const Win32ApplicationException& e) {
-		std::cerr << e.what() << "; Error code: " << e.getErrorCode() << std::endl;
-		std::ostringstream os;
-		os << "A windows application error has occurred. Error code: " << e.getErrorCode();
-		auto msgBoxMsg = os.str();
-		MessageBox(nullptr, std::wstring(msgBoxMsg.begin(), msgBoxMsg.end()).c_str(), L"Win32 Application Error", MB_OK);
+	} catch (const Win32ApplicationException& e) {
+		logErrorAndNotifyUser(e.what(), "A windows application error has occurred. Error code " + std::to_string(e.getErrorCode()));
 		return EXIT_FAILURE;
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		MessageBox(nullptr, L"An unknown error has occurred", L"Unknown Error", MB_OK);
+	} catch (const std::exception& e) {
+		logErrorAndNotifyUser(e.what(), "An unknown error has occurred.");
 		return EXIT_FAILURE;
 	}
 
