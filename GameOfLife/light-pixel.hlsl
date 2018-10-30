0f9065d96b0fda28c8c953f285c13755e50630e2
Maria Camenzuli <maria.camenzuli93@gmail.com>
2018-10-30 14:41:50 +0000

    Extract shadow detection into a method

--- GameOfLife/light-pixel.hlsl
+++ GameOfLife/light-pixel.hlsl
@@ -41,9 +41,45 @@ float4 sceneAmbience(PixelDescriptor pixel) {
     }
 }
 
+bool isInPointLightShadow(PixelDescriptor pixel) {
+    // Calculate the projected texture coordinates.
+    float2 projectTexCoord;
+    projectTexCoord.x = pixel.lightViewPosition.x / pixel.lightViewPosition.w / 2.0f + 0.5f;
+    projectTexCoord.y = -pixel.lightViewPosition.y / pixel.lightViewPosition.w / 2.0f + 0.5f;
+
+    // Determine if the projected coordinates are in the 0 to 1 range.  If so then this pixel is in the view of the light.
+    if ((saturate(projectTexCoord.x) == projectTexCoord.x) && (saturate(projectTexCoord.y) == projectTexCoord.y)) {
+        // Sample the shadow map depth value from the depth texture using the sampler at the projected texture coordinate location.
+        float shadowMapDepthValue = depthMapTexture.Sample(depthMapSampler, projectTexCoord).r;
+
+        // Calculate the depth of the light.
+        float pixelDepthValue = pixel.lightViewPosition.z / pixel.lightViewPosition.w;
+
+        // Set the bias value for fixing the floating point precision issues.
+        float bias = 0.000025f;
+
+        // Subtract the bias from the lightDepthValue.
+        pixelDepthValue = pixelDepthValue - bias;
+
+        // Compare the depth of the shadow map value and the depth of the light to determine whether to shadow or to light this pixel.
+        // If the light is in front of the object then light the pixel, if not then shadow this pixel since an object (occluder) is casting a shadow on it.
+        if (pixelDepthValue < shadowMapDepthValue) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return true;
+    }
+}
+
 float4 pointLight(PixelDescriptor pixel) {
     float4 color = 0;
     float lightIntensity;
+
+    if (isInPointLightShadow(pixel)) {
+        return color;
+    }
 
     float3 pointToLightVector = pointLightPosition - pixel.worldSpacePosition;
     float pointToLightVectorLength = length(pointToLightVector);
@@ -75,32 +111,8 @@ float4 colorPixel(PixelDescriptor pixel) : SV_TARGET {
     // start out with the ambient color
     pixelColor += sceneAmbience(pixel);
 
-    // Calculate the projected texture coordinates.
-    float2 projectTexCoord;
-    projectTexCoord.x = pixel.lightViewPosition.x / pixel.lightViewPosition.w / 2.0f + 0.5f;
-    projectTexCoord.y = -pixel.lightViewPosition.y / pixel.lightViewPosition.w / 2.0f + 0.5f;
-
-    // Determine if the projected coordinates are in the 0 to 1 range.  If so then this pixel is in the view of the light.
-    if ((saturate(projectTexCoord.x) == projectTexCoord.x) && (saturate(projectTexCoord.y) == projectTexCoord.y)) {
-        // Sample the shadow map depth value from the depth texture using the sampler at the projected texture coordinate location.
-        float shadowMapDepthValue = depthMapTexture.Sample(depthMapSampler, projectTexCoord).r;
-
-        // Calculate the depth of the light.
-        float pixelDepthValue = pixel.lightViewPosition.z / pixel.lightViewPosition.w;
-
-        // Set the bias value for fixing the floating point precision issues.
-        float bias = 0.000025f;
-
-        // Subtract the bias from the lightDepthValue.
-        pixelDepthValue = pixelDepthValue - bias;
-
-        // Compare the depth of the shadow map value and the depth of the light to determine whether to shadow or to light this pixel.
-        // If the light is in front of the object then light the pixel, if not then shadow this pixel since an object (occluder) is casting a shadow on it.
-        if (pixelDepthValue < shadowMapDepthValue) {
-            // add any diffuse light from point light
-            pixelColor += pointLight(pixel);
-        }
-    }
+    // add any diffuse light from point light
+    pixelColor += pointLight(pixel);
 
     return pixelColor;
 }
